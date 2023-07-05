36fba786b68a4b3fe9e6b952b4f9191c6ad86bbb
zhohn0 <540583339@qq.com>
2023-07-05 14:25:38 +0800

    Update train params.

--- finetune.sh
+++ finetune.sh
@@ -1,12 +1,12 @@
 python finetune.py \
     --dataset_path /home/aurobit/llm/data/chatglm-tuning/sens_all \
-    --lora_rank 8 \
-    --per_device_train_batch_size 8 \
+    --lora_rank 16 \
+    --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --max_steps 2000 \
     --save_steps 500 \
     --save_total_limit 5 \
-    --learning_rate 2e-6 \
+    --learning_rate 2e-5 \
     --fp16 \
     --remove_unused_columns false \
     --logging_steps 20 \
