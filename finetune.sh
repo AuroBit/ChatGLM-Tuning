0676e33fc19b8bb2a24203c67bd6fab336e39da9
zhohn0 <540583339@qq.com>
2023-07-10 16:35:11 +0800

    Update.

--- finetune.sh
+++ finetune.sh
@@ -1,14 +1,13 @@
 python finetune.py \
     --dataset_path ../data/chatglm-tuning/sens-chat-multiple-all \
-    --lora_rank 8 \
+    --lora_rank 16 \
     --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 1 \
-    --max_steps 2000 \
+    --max_steps 3000 \
     --save_steps 500 \
     --save_total_limit 5 \
     --learning_rate 2e-5 \
     --fp16 \
     --remove_unused_columns false \
     --logging_steps 20 \
-    --output_dir ../model/chatglm-tuning/sens-chat-multiple-all-0710-0 \
-    --model_version 2
+    --output_dir ../model/chatglm-tuning/sens-chat-multiple-all-0710-0
