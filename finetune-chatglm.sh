ca14f59b89de7c199a6c22c5bf2ac701f7acd792
zhohn0 <540583339@qq.com>
2023-07-13 20:34:21 +0800

    Update.

--- finetune-chatglm.sh
+++ finetune-chatglm.sh
@@ -1,7 +1,7 @@
 python finetune-chatglm.py \
     --base_model_path ../model/chatglm-6b \
     --dataset_path ../data/chatglm-tuning/chatglm-chat-all \
-    --output_dir ../model/chatglm-tuning/chatglm-chat-all-0713-3 \
+    --output_dir ../model/chatglm-tuning/chatglm-chat-all-0713-4 \
     --lora_rank 32 \
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 2 \
