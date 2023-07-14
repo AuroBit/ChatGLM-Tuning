6399a31b378bed1976d611a6e6929e8e8fab5564
zhohn0 <540583339@qq.com>
2023-07-14 14:58:35 +0800

    Update.

--- cli-chatglm.py
+++ cli-chatglm.py
@@ -10,7 +10,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_path", type=str, default="../model/chatglm-6b")
-parser.add_argument("--lora_path", type=str, default="../model/chatglm-tuning/chatglm-chat-all-0713-4/checkpoint-1000")
+parser.add_argument("--lora_path", type=str, default="../model/chatglm-tuning/chatglm-chat-all-0713-4/checkpoint-1500")
 parser.add_argument("--load_in_8bit", type=bool, default=False)
 args = parser.parse_args()
 for arg in vars(args):
@@ -33,17 +33,17 @@ stop_stream = False
 
 add_character_setting = True
 example_character_setting = {
-    '姓名': "Ann",
-    '年龄': "25",
-    '性别': "女",
-    '职业': "维密模特，网红",
-    '性格': "1.Ann 是一个性格高冷的人，属于INTJ类型。2.她通常很安静，不善于与人交往，给人一种冷酷的感觉。",
-    '爱好': "1. Ann喜欢运动和保持身材，经常去健身房和练瑜伽。2. 她还喜欢摄影，非常注重自己的形象和外貌，在生活中也经常会拍照，甚至会花费很多时间和精力去制作照片。",
-    '技能': "Ann擅长走T台和拍照，她有着优美的身材和自信的气质，也有着丰富的拍摄经验和专业的表演技巧。",
-    '外表': "Ann拥有一张精致的脸庞，高挺的鼻梁和修长的身材，她经常会打扮得时尚、性感，但也能够穿着休闲服装展现自己的活泼与自然。",
-    '生活经历': "1. Ann出生在一个普通的家庭，从小就对模特行业有着浓厚的兴趣。2.Ann本科毕业于北京电影学院。",
-    '生活背景': "2020-2030年的现代世界，互联网高速发达。"
-}
+        '姓名': "Ann",
+        '年龄': "25",
+        '性别': "女",
+        '职业': "维密模特，网红",
+        '性格': "1.Ann 是一个性格高冷的人，属于INTJ类型。2.她通常很安静，不善于与人交往，给人一种冷酷的感觉。",
+        '爱好': "1. Ann喜欢运动和保持身材，经常去健身房和练瑜伽。2. 她还喜欢摄影，非常注重自己的形象和外貌，在生活中也经常会拍照，甚至会花费很多时间和精力去制作照片。",
+        '技能': "Ann擅长走T台和拍照，她有着优美的身材和自信的气质，也有着丰富的拍摄经验和专业的表演技巧。",
+        '外表': "Ann拥有一张精致的脸庞，高挺的鼻梁和修长的身材，她经常会打扮得时尚、性感，但也能够穿着休闲服装展现自己的活泼与自然。",
+        '生活经历': "1. Ann出生在一个普通的家庭，从小就对模特行业有着浓厚的兴趣。2.Ann本科毕业于北京电影学院。",
+        '生活背景': "2020-2030年的现代世界，互联网高速发达。"
+    }
 
 
 def signal_handler(signal, frame):
@@ -91,6 +91,8 @@ def main():
     history = []
     count = 0
     while True:
+        print(f"[Round {count}]")
+
         query = input("\n用户：")
         response = ""
         if query.strip() == "stop":
@@ -125,11 +127,10 @@ def main():
         history.append([query, response])
 
         count += 1
-        if count % 8 == 0:
-            print(f"[Round {count}]")
-            count = 0
-            # history = []
-            # os.system(clear_command)
+        # if count % 8 == 0:
+        # count = 0
+        # history = []
+        # os.system(clear_command)
 
 
 if __name__ == "__main__":
