c44804a81d7c3401c9537e56df880ece56d21f3c
zhohn0 <540583339@qq.com>
2023-07-17 11:16:03 +0800

    Update.

--- cli-chatglm.py
+++ cli-chatglm.py
@@ -10,7 +10,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_path", type=str, default="../model/chatglm-6b")
-parser.add_argument("--lora_path", type=str, default="../model/chatglm-tuning/chatglm-chat-all-0713-4/checkpoint-2000")
+parser.add_argument("--lora_path", type=str, default="../model/chatglm-tuning/chatglm-chat-all-0713-4/checkpoint-1000")
 parser.add_argument("--load_in_8bit", type=bool, default=False)
 parser.add_argument("--temperature", type=float, default=0.98)
 args = parser.parse_args()
